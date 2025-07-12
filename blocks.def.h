87190583e3f833226195a226adea80f2d67eb1c6
Salinas <80262440+eoSalinas@users.noreply.github.com>
2025-07-12 00:21:28 -0400

    initial version with build fixes
    Fixed two compiler warnings/errors:
    - int parameter signal
    - variable defined but never used

---
+++ blocks.def.h
@@ -0,0 +1,11 @@
+
+static const Block blocks[] = {
+	/*Icon, Command, Update Interval, Update Signal*/
+    {"cpu: ", "top -bn1 | grep 'Cpu(s)' | sed 's/.*, *\\([0-9.]*\\)%* id.*/\\1/' | awk '{print 100 - $1 \"%\"}'", 3, 0},
+	{"ram: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 5, 0},
+    {"battery: ", "cat /sys/class/power_supply/BAT0/capacity | awk '{print $1 \"%\"}'", 30, 0},
+	{"", "date '+%B %d - %I:%M%P '", 60, 0},
+};
+
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
