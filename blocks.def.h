71b95631937dbf840da83c9703ab9c4e49b0cdd0
Salinas <80262440+eoSalinas@users.noreply.github.com>
2025-07-13 17:51:39 -0400

    refactor: support `XDG` base directory for script paths (#2)
    This PR refactors **dwmblocks** to use the [XDG Base
    Directory](https://wiki.archlinux.org/title/XDG_Base_Directory)
    specification for locating script files, ensuring better compatibility
    with modern Linux environments.
    
    ### Changes:
    - Replaced hardcoded paths (e.g., `$HOME/scripts/`) with XDG-compliant
    paths (e.g., `$XDG_CONFIG_HOME/scripts/`).
    - Improved flexibility and compatibility with user-configured
    directories for configuration and scripts.
    
    #### Additional Notes:
    If you’re setting up your environment to fully use the XDG Base
    Directory, you might also want to ensure the XDG environment variables
    are configured.
    
    For setting these up, check out the related [repository with
    .zprofile](https://github.com/eoSalinas/dots/blob/main/.zprofile) where
    these variables can be set globally.

--- blocks.def.h
+++ blocks.def.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon, Command, Update Interval, Update Signal*/
     {"cpu: ", "top -bn1 | grep 'Cpu(s)' | sed 's/.*, *\\([0-9.]*\\)%* id.*/\\1/' | awk '{print 100 - $1 \"%\"}'", 3, 0},
 	{"ram: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 5, 0},
-    {"battery: ", "$HOME/scripts/dwmblocks-battery", 30, 0},
+    {"battery: ", "$XDG_CONFIG_HOME/scripts/dwmblocks-battery", 30, 0},
 	{"", "date '+%B %d - %I:%M%P '", 60, 0},
 };
 
